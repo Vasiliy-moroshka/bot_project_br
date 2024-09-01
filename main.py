93fbe2257f6a7d8eeb4a691a378b8e6cd1d08ce5
Василий <Wh1lepp@gmail.com>
2024-09-01 18:53:12 +0200

    Added func to greeting user and tell his id

--- main.py
+++ main.py
@@ -12,4 +12,11 @@ def main(message):
 def main(message):
     bot.send_message(message.chat.id, '<b>Help</b> <em><u>information</u></em>', parse_mode='html')
 
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID, {message.from_user.id}')
+
 bot.polling(none_stop=True)
