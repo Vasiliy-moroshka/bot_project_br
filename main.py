0de14f43b28ea8e9389f588e2db732c00a33b3cb
Василий <Wh1lepp@gmail.com>
2024-09-01 14:02:52 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+
+bot = telebot.TeleBot('7228654217:AAERzZ662cate8coueeC__DFm8Xbg0_CQb8')
+
+@bot.message_handler(commands=['start', 'hello', 'main'])
+
+def main(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+@bot.message_handler(commands=['help'])
+
+def main(message):
+    bot.send_message(message.chat.id, '<b>Help</b> <em><u>information</u></em>', parse_mode='html')
+
+bot.polling(none_stop=True)
